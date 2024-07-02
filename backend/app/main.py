be659aa381085630fe5c0b93f161ec0921b7fbef
Ashley Boswell <boswelas@oregonstate.edu>
2024-07-02 12:58:38 -0700

    revert port num

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,8 +16,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 DATABASE_HOST = os.getenv('DB_HOST')
-# DATABASE_PORT = os.getenv('DB_PORT', default=5432)
-DATABASE_PORT = 5432
+DATABASE_PORT = os.getenv('DB_PORT')
 DATABASE_NAME = os.getenv('DB_NAME')
 DATABASE_USER = os.getenv('DB_USER')
 DATABASE_PASSWORD = os.getenv('DB_PASS')
