536e98a4369324ebc8b256310bda23e997fdeef4
Ashley Boswell <boswelas@oregonstate.edu>
2024-07-02 11:13:49 -0700

    strip port

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config['DATABASE_HOST'] = os.getenv('DB_HOST')
-app.config['DATABASE_PORT'] = int(os.getenv('DB_PORT'))
+app.config['DATABASE_PORT'] = int(os.getenv('DB_PORT').strip())
 app.config['DATABASE_NAME'] = os.getenv('DB_NAME')
 app.config['DATABASE_USER'] = os.getenv('DB_USER')
 app.config['DATABASE_PASSWORD'] = os.getenv('DB_PASS')
