5c6c4f12d0d55c9f05e4a9f068677139d9cdf0b9
Ashley Boswell <boswelas@oregonstate.edu>
2024-07-02 10:39:06 -0700

    reconfigured next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,14 @@ const nextConfig = {
             { hostname: "img.icons8.com" }
         ]
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://starwarsbattle-production.up.railway.app/:path*' // Proxy to Flask backend
+            }
+        ]
+    }
 };
 
 export default nextConfig;
