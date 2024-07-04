4c2cea63e722f22f3161e48f5f28e8efcd6e4524
Ashley Boswell <boswelas@oregonstate.edu>
2024-07-04 11:27:49 -0700

    added condition for matching char1 and char2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,47 +13,51 @@ export default function CharacterPage() {
   const router = useRouter();
 
   const handleBattle = () => {
-    if (character1 && character2) {
-      router.push(`/battle?character1=${encodeURIComponent(character1)}&character2=${encodeURIComponent(character2)}`);
-    } else {
+    if (!character1 || !character2) {
       setError('Both characters must be selected before starting the battle.');
+    } else if (character1 === character2) {
+      setError('Characters cannot battle themselves.');
+    }
+    else {
+      router.push(`/battle?character1=${encodeURIComponent(character1)}&character2=${encodeURIComponent(character2)}`);
     }
   };
 
-  const handleCloseInfoBox = () => {
-    setShowInfoBox(false);
-  };
 
-  return (
-    <div className='flex flex-col items-center h-screen max-w-screen overflow-y-scroll no-scrollbar 
+const handleCloseInfoBox = () => {
+  setShowInfoBox(false);
+};
+
+return (
+  <div className='flex flex-col items-center h-screen max-w-screen overflow-y-scroll no-scrollbar 
     bg-cover bg-center' style={{ backgroundImage: `url('/images/space.jpg')` }}>
-      <div className='flex flex-col items-center'>
-        <h1 className='mt-10 text-[#FFFF00] custom-heading'>
-          Star Wars
-        </h1>
-        <h2 className='text-xl mb-5 text-neutral-500 custom-heading2'>Character Battle</h2>
+    <div className='flex flex-col items-center'>
+      <h1 className='mt-10 text-[#FFFF00] custom-heading'>
+        Star Wars
+      </h1>
+      <h2 className='text-xl mb-5 text-neutral-500 custom-heading2'>Character Battle</h2>
 
-        <div className='flex items-center justify-center'>
-          <Image src={'/images/x-wing.png'} height={30} width={30} alt="" className='mr-5 mt-2'></Image>
-          <button
-            className="mt-3 primary-btn h-10 text-lg bg-red-700 p-3 rounded-md w-28  flex items-center justify-center hover:bg-red-600"
-            onClick={handleBattle}
-          >
-            Battle
-          </button>
-          <Image src={'/images/tie-fighter.png'} height={30} width={30} alt="" className='ml-5 mt-2'></Image>
-        </div>
-        {error && <p className="text-red-600 mt-2 ">{error}</p>}
-        <div className='flex flex-row gap-36 mt-10'>
-          <SearchBar onSelect={setCharacter1} />
-          <SearchBar onSelect={setCharacter2} />
-        </div>
-        <div>
-          <InfoBox onClose={handleCloseInfoBox} />
-        </div>
-
-
+      <div className='flex items-center justify-center'>
+        <Image src={'/images/x-wing.png'} height={30} width={30} alt="" className='mr-5 mt-2'></Image>
+        <button
+          className="mt-3 primary-btn h-10 text-lg bg-red-700 p-3 rounded-md w-28  flex items-center justify-center hover:bg-red-600"
+          onClick={handleBattle}
+        >
+          Battle
+        </button>
+        <Image src={'/images/tie-fighter.png'} height={30} width={30} alt="" className='ml-5 mt-2'></Image>
       </div>
+      {error && <p className="text-red-600 mt-2 ">{error}</p>}
+      <div className='flex flex-row gap-36 mt-10'>
+        <SearchBar onSelect={setCharacter1} />
+        <SearchBar onSelect={setCharacter2} />
+      </div>
+      <div>
+        <InfoBox onClose={handleCloseInfoBox} />
+      </div>
+
+
     </div>
-  );
+  </div>
+);
 }
